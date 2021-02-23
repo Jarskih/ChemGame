3b86b3237981f9e8299a507d828bf7bd67f012c2
Jari <madcook86@gmail.com>
2021-02-23 14:18:09 +0100

    Create project

---
+++ Assets/Scripts/Attractor.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Attractor : MonoBehaviour
+{
+    private const float G = 667.4f;
+
+    public static List<Attractor> Attractors;
+    public bool IsNegativeCharge;
+
+    public bool UseGravityBorder = false;
+
+    [SerializeField] private bool ReactOnTouch = false;
+
+    public Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (UseGravityBorder)
+        {
+            return;
+        }
+
+        foreach (Attractor attractor in Attractors)
+        {
+            if (attractor != this)
+            {
+                if (attractor.IsNegativeCharge && !IsNegativeCharge)
+                {
+                    Attract(attractor, true);
+                }
+                else
+                {
+                    Attract(attractor, false);
+                }
+            }
+        }
+    }
+
+    private void OnEnable()
+    {
+        if (Attractors == null)
+        {
+            Attractors = new List<Attractor>();
+        }
+
+        Attractors.Add(this);
+    }
+
+    private void OnDisable()
+    {
+        Attractors.Remove(this);
+    }
+    
+    private void Attract(Attractor objToAttract, bool towards)
+    {
+        Rigidbody rbToAttract = objToAttract.rb;
+
+        if (rbToAttract == null)
+        {
+            return;
+        }
+
+
+        Vector3 direction = new Vector3();
+        if (towards)
+        {
+            direction = rb.position - rbToAttract.position;
+        }
+        else
+        {
+            direction = rbToAttract.position - rb.position;
+        }
+        
+        float distance = direction.magnitude;
+
+        if (distance <= 0.1f)
+        {
+            return;
+        }
+
+        float forceMagnitude = G * (rb.mass * rbToAttract.mass) / Mathf.Pow(distance, 2);
+        Vector3 force = direction.normalized * forceMagnitude;
+        
+        rbToAttract.AddForce(force);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (UseGravityBorder)
+        {
+            var attractor = other.GetComponent<Attractor>();
+            if (attractor != null)
+            {
+                if (attractor.IsNegativeCharge && !IsNegativeCharge)
+                {
+                    Attract(attractor, true);
+                }
+                else
+                {
+                    Attract(attractor, false);
+                }
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (ReactOnTouch)
+        {
+            Debug.Log("reaction");
+        }
+    }
+}
---
+++ Assets/Scripts/Controls.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+    [SerializeField] private float _force = 1f;
+    [SerializeField] private float _rotationForce = 1f;
+
+    private float _timer = 1;
+    private float _delay = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        _timer += Time.deltaTime;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            _rigidbody.AddForce(transform.right * _force, ForceMode.Acceleration);
+            
+            if (_timer > _delay)
+            {
+                _timer = 0;
+                //_rigidbody.AddForce(transform.right * _force, ForceMode.Acceleration);
+            }
+        }
+
+        float turn = Input.GetAxis("Horizontal");
+
+        _rigidbody.AddTorque(transform.forward * turn * _rotationForce);
+        }
+}
