e5791bb21e4060e4f191e3c4cc4358d95e66ae63
Jari <madcook86@gmail.com>
2021-02-23 16:17:52 +0100

    Bouncy thing

---
+++ Assets/Scripts/SquashOnCollision.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class SquashOnCollision : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other)
+    {
+        transform.localScale = new Vector3(1, 1, 1);
+        
+        ContactPoint contact = other.contacts[0];
+        Vector3 myCollisionNormal = other.contacts[0].normal;
+        myCollisionNormal = new Vector3(Mathf.Abs(myCollisionNormal.x), Mathf.Abs(myCollisionNormal.y), 1);
+        Debug.Log("normal: " + myCollisionNormal);
+        var value = new Vector3(Mathf.Clamp(myCollisionNormal.x, 0.8f, 1.2f), Mathf.Clamp(myCollisionNormal.y, 0.8f, 1.2f), 1);
+        
+        Sequence mySequence = DOTween.Sequence();
+        mySequence.Append(transform.DOScale(value, 0.1f))
+            .Append(transform.DOScale(new Vector3(1, 1, 1), 0.2f));
+    }
+}
