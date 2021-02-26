f592b0cc9310cc9c2134a860e8f0581c5aa5b8c6
Pontus Jakobsson <jakobsson.pontus@gmail.com>
2021-02-26 13:01:44 +0100

    Uploaded sound effects and music and added sound manager

--- Assets/Scripts/SquashOnCollision.cs
+++ Assets/Scripts/SquashOnCollision.cs
@@ -9,7 +9,8 @@ public class SquashOnCollision : MonoBehaviour
     private void OnCollisionEnter(Collision other)
     {
         transform.localScale = new Vector3(1, 1, 1);
-        
+        SoundManagerScript.PlaySound("Collision");
+
         ContactPoint contact = other.contacts[0];
         Vector3 myCollisionNormal = other.contacts[0].normal;
         myCollisionNormal = new Vector3(Mathf.Abs(myCollisionNormal.x), Mathf.Abs(myCollisionNormal.y), 1);
@@ -19,5 +20,7 @@ public class SquashOnCollision : MonoBehaviour
         Sequence mySequence = DOTween.Sequence();
         mySequence.Append(transform.DOScale(value, 0.1f))
             .Append(transform.DOScale(new Vector3(1, 1, 1), 0.2f));
+
+    
     }
 }
